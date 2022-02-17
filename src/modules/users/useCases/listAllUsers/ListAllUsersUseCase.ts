af9e50e4c8af2f9db25adaa405d8e6f0fa419668
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2022-02-17 18:55:05 -0300

    add estrutura inicial

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,26 @@
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if (!foundUser) {
+      throw new Error('User does not exist!');
+    }
+
+    if (!foundUser.admin) {
+      throw new Error('User provided is not an admin!');
+    }
+
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const userFound = this.usersRepository.findById(user_id);
+
+    if (!userFound) {
+      throw new Error('User does not exist!');
+    }
+
+    userFound.admin = true;
+
+    return userFound;
+  }
+}
+
+export { TurnUserAdminUseCase };
